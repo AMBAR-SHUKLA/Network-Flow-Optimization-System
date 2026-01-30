ae42f221793122d29e378d6848157f3ac98c805d
AMBAR-SHUKLA <AMBAR.12221174@lpu.in>
2026-01-30 19:21:32 +0530

    Implement placeholder for Gomory-Hu tree function
    Added a placeholder function for Gomory-Hu tree implementation.

---
+++ src/engine/src/engine/src/engine/gomory_hu.cpp
@@ -0,0 +1,10 @@
+#include "graph.hpp"
+#include <vector>
+#include <iostream>
+using namespace std;
+
+vector<pair<int,int>> gomoryHuTree(Graph &g) {
+    vector<pair<int,int>> treeEdges;
+    cout << "Gomory-Hu tree placeholder. Implement if needed.\n";
+    return treeEdges;
+}
